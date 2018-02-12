fc7915fc7fcaa510a1212161c67b9dc0d7cad178
David Salomon <david.salomon@arxes-tolina.de>
2018-02-12 08:22:57 +0100

    Erkennung der Aliasannotation erweitert

--- src/test/java/de/tolina/common/validation/AliasTestAnnotation.java
+++ src/test/java/de/tolina/common/validation/AliasTestAnnotation.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016 arxes-tolina GmbH (entwicklung@arxes-tolina.de)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,17 +15,21 @@
  */
 package de.tolina.common.validation;
 
-import static de.tolina.common.validation.TestEnum.TEST;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.Retention;
 
-import org.springframework.core.annotation.AliasFor;
+import static de.tolina.common.validation.TestEnum.TEST;
+import static de.tolina.common.validation.TestEnum.TEST2;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
 @SuppressWarnings("javadoc")
 @AnotherTestAnnotation
 public @interface AliasTestAnnotation {
-	@AliasFor(annotation = AnotherTestAnnotation.class, attribute = "testEnum")
-	TestEnum referencedTestEnum() default TEST;
+    @AliasFor(annotation = AnotherTestAnnotation.class, attribute = "testEnum")
+    TestEnum referencedTestEnum() default TEST;
+
+    @AliasFor(annotation = AnotherTestAnnotation.class)
+    TestEnum anotherValue() default TEST2;
 }
--- src/test/java/de/tolina/common/validation/AnnotatedTestClass.java
+++ src/test/java/de/tolina/common/validation/AnnotatedTestClass.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016 arxes-tolina GmbH (entwicklung@arxes-tolina.de)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,47 +22,52 @@ import static de.tolina.common.validation.TestEnum.TEST2;
 @SuppressWarnings("javadoc")
 class AnnotatedTestClass extends AnnotatedAbstractTestClass implements AnnotatedTestInterface {
 
-	@TestAnnotation(testparameter = "testvalue")
-	private String fieldWithAnnotations;
+    @TestAnnotation(testparameter = "testvalue")
+    private String fieldWithAnnotations;
 
-	String fieldWithoutAnnotations;
+    String fieldWithoutAnnotations;
 
-	@TestAnnotation(testparameter = "testvalue", anotherTestParameter = "anotherTestValue")
-	@AnotherTestAnnotation(TEST2)
-	public void methodWithAnnotations() {
-		// noop
-	}
+    @TestAnnotation(testparameter = "testvalue", anotherTestParameter = "anotherTestValue")
+    @AnotherTestAnnotation(TEST2)
+    public void methodWithAnnotations() {
+        // noop
+    }
 
-	@AliasTestAnnotation(referencedTestEnum = TEST2)
-	public void methodWithAliasAnnotations() {
-		// noop
-	}
+    @AliasTestAnnotation(referencedTestEnum = TEST2)
+    public void methodWithAliasAnnotations() {
+        // noop
+    }
 
-	public void methodWithoutAnnotations() {
-		// noop
-	}
+    @AliasTestAnnotation(anotherValue = TEST)
+    public void methodWithAnOtherAliasAnnotations() {
+        // noop
+    }
 
-	@Override
-	public void annotatedInterfaceMethod() {
-		// noop
-	}
+    public void methodWithoutAnnotations() {
+        // noop
+    }
 
-	@Override
-	public void annotatedInterfaceMethodForAbstractClass() {
-		// noop
-	}
+    @Override
+    public void annotatedInterfaceMethod() {
+        // noop
+    }
 
-	@Override
-	protected void annotatedAbstractMethod() {
-		// noop
-	}
+    @Override
+    public void annotatedInterfaceMethodForAbstractClass() {
+        // noop
+    }
 
-	@AnotherTestAnnotation(TEST)
-	public void overloadedMethod(final String foo, final String bar) {
-		// noop
-	}
+    @Override
+    protected void annotatedAbstractMethod() {
+        // noop
+    }
 
-	public void overloadedMethod(final String foo, final int bar) {
-		// noop
-	}
+    @AnotherTestAnnotation(TEST)
+    public void overloadedMethod(final String foo, final String bar) {
+        // noop
+    }
+
+    public void overloadedMethod(final String foo, final int bar) {
+        // noop
+    }
 }
