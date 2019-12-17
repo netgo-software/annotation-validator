970e6fc2e2d3bcd20dfc51fc33e2485bfd8420a4
Frank Jakop <frank.jakop@f-i-solutions-plus.de>
2019-12-17 15:50:46 +0100

    bump dependencies to ensure Java 11 compatibility

--- src/test/java/de/tolina/common/validation/AnnotationValidatorTest.java
+++ src/test/java/de/tolina/common/validation/AnnotationValidatorTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016 arxes-tolina GmbH (entwicklung@arxes-tolina.de)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,17 +15,17 @@
  */
 package de.tolina.common.validation;
 
-import static de.tolina.common.validation.AnnotationValidator.validate;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.junit.Assert.assertThat;
-
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import static de.tolina.common.validation.AnnotationValidator.validate;
+import static org.junit.Assert.assertThat;
+
 public class AnnotationValidatorTest {
-	@Test
-	public void testValidate_DefaultBlacklist() {
-		final AnnotationValidation validate = validate();
-		assertThat(validate.paramBlacklist, containsInAnyOrder("equals", "toString", "hashCode", "annotationType"));
-	}
+    @Test
+    public void testValidate_DefaultBlacklist() {
+        final AnnotationValidation validate = validate();
+        assertThat(validate.paramBlacklist, CoreMatchers.hasItems("equals", "toString", "hashCode", "annotationType"));
+    }
 
 }
